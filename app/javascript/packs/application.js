0824ed99aad2eb8a8d0dd093443eb50a7e63e1e0
Amelia <gcab022111@coderacademy.edu.au>
2022-03-08 15:12:51 +1000

    removed all bootstap manual installation to try and fix heroku error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
+
 // = require jquery3
 // = require popper
+// = require bootstrap-sprockets
 require("trix")
 require("@rails/actiontext")
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
-// manual install of bootstrap
-const webpack = require('webpack')
-environment.plugins.append('Provide'),
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-    
-
-
 
 module.exports = environment
