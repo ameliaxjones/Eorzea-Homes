3a167414a6a2fd3e916efbfaac64697c70064d23
Amelia <gcab022111@coderacademy.edu.au>
2022-03-10 11:50:37 +1000

    styled edit and new form, fixed issue with sellers being able to post new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -16,7 +16,8 @@ class ListingsController < ApplicationController
 
   # GET /listings/new
   def new
-    @listing = Listing.new
+    
+    @listing = current_user.listings.build
   end
 
   # GET /listings/1/edit
@@ -25,7 +26,7 @@ class ListingsController < ApplicationController
 
   # POST /listings or /listings.json
   def create
-    @listing = Listing.new(listing_params)
+    @listing = current_user.listings.build(listing_params)
 
     respond_to do |format|
       if @listing.save
