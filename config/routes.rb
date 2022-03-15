8580844799aa22ba1d8eb28a0ef99b097198f14b
Amelia <gcab022111@coderacademy.edu.au>
2022-03-15 12:21:38 +1000

    installed stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   resources :listings
   resources :game_addresses
   resources :h_cats
+  resources :charges
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get "pages/how"
   get "pages/designers"
   get "/my_listings", to: "listings#my_listings"
+
   
 end
