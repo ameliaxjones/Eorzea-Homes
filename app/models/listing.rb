d03a2af9c74005d4a6e5e13237b220515fbc5329
Amelia <gcab022111@coderacademy.edu.au>
2022-03-15 16:15:25 +1000

    added validation to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
-
+  validates :price, presence: true
+  validates :title, length: {minimum: 3, maximum: 50}
+  validates :short_body, length: {minimum: 10}
   belongs_to :user
   has_many_attached :photos
 end
