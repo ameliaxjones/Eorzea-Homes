39ad5777665ecbf9eed0f1420b37fe4597b76045
Amelia <gcab022111@coderacademy.edu.au>
2022-03-15 16:05:07 +1000

    working through stripe bug - stripe is working frm this point

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,6 @@
+Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_51KRPKoHBQHuZKEECOZIwWNPNJtX5Rse9mSktKYFobu20O5fQiTnDVR5gTzvoKMBTFnudLEhA19WmVTYuddqQGH7100Sm5AgkAt',
+    :secret_key => 'sk_test_51KRPKoHBQHuZKEECmBzkvDVEhctPimlgkfFyJCdmGX4tbgtCK4BUhNIRwBK4WRTP5AWN7DwIfkQRjoSMdwv1v3LQ00Syk65vKL'
+    }
+
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
