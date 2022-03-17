cf529ba60dc9a91207f643c9571f0a09482093ec
Amelia <gcab022111@coderacademy.edu.au>
2022-03-17 13:07:28 +1000

    seeded yukkio files

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,23 @@ user = User.create(email: "test@maildrop.cc", password: "123456", password_confi
 buyer = User.create(email: "buyer@maildrop.cc", password: "123456", password_confirmation: "123456", buyer: true)
 seller = User.create(email: "seller@maildrop.cc", password: "123456", password_confirmation: "123456", seller: true)
 
-10.times do
+2.times do
 Listing.create(title: "my house", short_body: "hfgiuhwiasheiunvug hruigaiusbvoufih iurgpa", user_id: seller.id, price: 300, open: true, housingsize_id: large.id)
 end
 
-seller = User.create(email: yokkio@maildrop.cc,
-                    password: "123456,"
+yokkio = User.create(email: "yokkio@maildrop.cc",
+                    password: "123456",
                     password_confirmation: "123456",
                     seller: true
-                    
-
-
-
-
-
 )
+yokkio.avatar.attach(io: File.open(Rails.root.join('app/assets/images/profiles/yokkio_med/profile.png')), filename: 'profile.png')
+
+yokkio_listing = Listing.create(title: "Med house ", short_body: "My hoyse is cool  iurgpa", user_id: yokkio.id, price: 300, open: true, housingsize_id: large.id)
+
+yokkio_listing.photos.attach(io: File.open(Rails.root.join('app/assets/images/profiles/yokkio_med/1.jpg')), filename: '1.jpg')
+
+yokkio_listing.photos.attach(io: File.open(Rails.root.join('app/assets/images/profiles/yokkio_med/2.jpg')), filename: '2.jpg')
+
+yokkio_listing.photos.attach(io: File.open(Rails.root.join('app/assets/images/profiles/yokkio_med/3.jpg')), filename: '3.jpg')
+
+yokkio_listing.photos.attach(io: File.open(Rails.root.join('app/assets/images/profiles/yokkio_med/4.jpg')), filename: '4.jpg')
