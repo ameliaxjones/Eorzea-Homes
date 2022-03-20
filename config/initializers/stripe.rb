6205afbe64361a2f8b726d8377ec6cbd69f23aa5
Amelia <gcab022111@coderacademy.edu.au>
2022-03-20 14:35:17 +1000

    fixed avatar review defect and secured stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,9 @@
 Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_51KRPKoHBQHuZKEECOZIwWNPNJtX5Rse9mSktKYFobu20O5fQiTnDVR5gTzvoKMBTFnudLEhA19WmVTYuddqQGH7100Sm5AgkAt',
-    :secret_key => 'sk_test_51KRPKoHBQHuZKEECmBzkvDVEhctPimlgkfFyJCdmGX4tbgtCK4BUhNIRwBK4WRTP5AWN7DwIfkQRjoSMdwv1v3LQ00Syk65vKL'
+    :publishable_key => Rails.application.credentials.stripe[:public_key],
+    
+    :secret_key => Rails.application.credentials.stripe[:secret_key]
     }
 
 Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+
